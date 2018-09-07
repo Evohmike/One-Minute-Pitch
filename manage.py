9b5dc5130d53200a0939033c438eb0c88bd5b530
Evohmike <evohmike@gmail.com>
2018-09-07 23:45:10 +0300

    modified manage file

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
+
+#connect to models
 from app.models import User
 
 
@@ -8,24 +10,18 @@ from app.models import User
 # Creating app instance
 app = create_app('development')
 
+#create manger instance
 manager = Manager(app)
-manager.add_command('server',Server)
 
-
-
-# Creating app instance
-app = create_app('development')
-
-manager = Manager(app)
 manager.add_command('server',Server)
 
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User, Role = Role )
+    return dict(app = app,db = db,User = User )
     
-
-
-
 if __name__ == '__main__':
     manager.run()
+
+
+
