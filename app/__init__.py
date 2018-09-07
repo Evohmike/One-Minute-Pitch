f0be16fe6cc8fd9b19cb29b1b4dd76ec97e41186
Evohmike <evohmike@gmail.com>
2018-09-07 15:42:44 +0300

    created app folder and files

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from config import config_options
+
+
+
+def create_app(config_name):
+
+  # Initializing application
+  app = Flask(__name__)
+
+  # Creating the app configurations
+  app.config.from_object(config_options[config_name])
+  
+
+  # Registering the main blueprint
+  from .main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
+
+  # Registering the auth blueprint
+  from .auth import auth as auth_blueprint
+  app.register_blueprint(auth_blueprint)
+
+ 
+  return app
---
+++ app/auth/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField
+from wtforms.validators import Required
+
+class RegisterForm(FlaskForm):
+	username = StringField('username',validators=[Required()])
+	password = PasswordField('password', validators=[Required()])
+	submit = SubmitField("submit")
+
+
+class LoginForm(FlaskForm):
+	username = StringField('username',validators=[Required()])
+	password = PasswordField('password', validators=[Required()])
+	remember = BooleanField("Remember me")
+	submit = SubmitField("submit")
