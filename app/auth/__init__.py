ccbfa06098811e41db3f5ad9bcca270fe26c5cb7
Evohmike <evohmike@gmail.com>
2018-09-07 18:43:58 +0300

    this modified manage file

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,3 +1,3 @@
 from flask import Blueprint
-auth = Blueprint('main',__name__)
-from . import views,error
+auth = Blueprint('auth',__name__)
+from . import views
--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
 from app.models import User
 
@@ -18,6 +18,15 @@ app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
+    
+if __name__ == '__main__':
+    manager.run()
+
 
 
 if __name__ == '__main__':
