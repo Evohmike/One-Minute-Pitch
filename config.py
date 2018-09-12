56feea68c5ae8b94526af29dc22472768e4c5994
Evohmike <evohmike@gmail.com>
2018-09-12 14:44:28 +0300

    deploy

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_WHITE_URL")
 
 
 class TestConfig(Config):
@@ -35,7 +35,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     # '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://evohmike:1234@localhost/pitches'
+    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://evohmike:1234@localhost/pitches'
 
     DEBUG = True
 
