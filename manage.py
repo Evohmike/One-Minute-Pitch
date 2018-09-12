6927df6ce66762e925a58615f334d28c0f9ea7e6
Evohmike <evohmike@gmail.com>
2018-09-12 12:05:42 +0300

    fixes

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from app.models import User, Role, Category,Pitch
 from app.models import User
 
 # Creating app instance
-app = create_app('production')
+app = create_app('development')
 
 #create manager instance
 manager = Manager(app)
@@ -29,28 +29,3 @@ if __name__ == '__main__':
 
 
 
-
-
-# # Import db from app factory
-# from app import create_app,db
-# from flask_script import Manager,Server
-# # Connect to models
-# from app.models import User,Post,Comment,Upvote,Downvote
-# # Set up migrations
-# from flask_migrate import Migrate,MigrateCommand
-
-# # Creating app instance
-# app = create_app('development')
-
-
-# # Create manager instance
-# manager = Manager(app)
-
-# # Create migrate instance
-# migrate = Migrate(app,db)
-
-# manager.add_command('server',Server)
-# manager.add_command('db',MigrateCommand)
-
-# if __name__ == '__main__':
-#     manager.run()
