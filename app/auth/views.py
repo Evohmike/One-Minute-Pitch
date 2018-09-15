4efb146419e50b14d45ed9bb11bedf280316d55a
Evohmike <evohmik@gmail.com>
2018-09-16 01:30:30 +0300

    added more styles to the templates.html

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,60 +1,4 @@
 
-# from flask import render_template,request,redirect,url_for,abort
-# from flask_login import login_user,login_required,current_user,logout_user
-# from ..models import User
-# from .forms import LoginForm,RegistrationForm
-# from . import auth
-
-# # Views
-# @auth.route('/login', methods=["GET","POST"])
-# def login():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('main.dashboard'))
-#     title = 'Login'
-#     Form = LoginForm()
-#     Error=False
-#     if Form.validate_on_submit():
-#         username=str(Form.username.data)
-#         password=str(Form.password.data)
-#         if username and password:
-#             user=User.query.filter(User.username==username).first()
-#             if user and user.verifypass(password):
-#                 print(password)
-#                 login_user(user,Form.remember.data)
-#                 return redirect(url_for('main.dashboard'))
-#             Error='Wrong Username or Password'
-#         else:
-#             Error='Please Type a Username or Password'
-#     return render_template('login.html', title = title ,Form=Form,Error=Error)
-
-# @auth.route('/register', methods=["GET","POST"])
-# def register():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('main.dashboard'))
-#     title = 'Register'
-#     Form = RegisterForm()
-#     Error=False
-#     if Form.validate_on_submit():
-#         username=str(Form.username.data)
-#         password=str(Form.password.data)
-#         if username and password:
-#             user=User.query.filter(User.username==username).first()
-#             if not user:
-#                 user=User(username=username,passwd=password)
-#                 user.save()
-#                 return redirect(url_for('auth.login'))
-#             Error='Username Already taken'
-#     return render_template('register.html', title = title ,Form=Form,Error=Error)
-
-# @auth.route('/logout')
-# def logout():
-#     if current_user.is_authenticated:
-#         logout_user()
-#     return redirect(url_for('main.dashboard'))
-
-
-
-
 
 from flask import render_template, redirect, url_for, request, flash
 from . import auth
@@ -90,11 +34,10 @@ def logout():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        user = User(email=form.email.data,
+                    username=form.username.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('auth.login'))
         title = 'New Account'
     return render_template('auth/register.html', registration_form=form)
-
-
