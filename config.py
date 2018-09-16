46721c650c24178da45e7f80c6162d5aa620eaa5
Evohmike <evohmik@gmail.com>
2018-09-16 16:49:35 +0300

    modified readme

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_ORANGE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_MAUVE_URL")
 
 
 class TestConfig(Config):
